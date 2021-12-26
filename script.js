1271a96bd7607eee64632925c3bb28202abe978b
21harley <jhonharleyllanesescobar@hotmail.com>
2021-12-25 23:05:57 -0400

    segundo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load",()=>{
         d.classList.add("estrella1");
         if((contador+1)==60) clearInterval(inter);
         contador++;
-    },10);
+    },100);
     let contador1=0;
     let inter1=setInterval(()=>{
         const container=document.querySelector("#container1");
@@ -14,7 +14,7 @@ window.addEventListener("load",()=>{
         d.classList.add("arbol1");
         if((contador1+1)==90) clearInterval(inter1);
         contador1++;
-    },11);
+    },110);
     
 })
 
